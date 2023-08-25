8d66b05c7de44c7df4aa9f4e6665e9a1a0695384
Toshimichi0915 <main@toshimichi.net>
2023-08-25 22:50:44 +0900

    update to use s3

--- src/pages/api/image.ts
+++ src/pages/api/image.ts
@@ -1,12 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { createReadStream } from "fs";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const file = createReadStream("./test.png");
-
+  const image = await fetch("https://toshimichi-public.s3.ap-northeast-1.amazonaws.com/icon.png")
   res.setHeader("Content-Type", "image/png");
-  file.pipe(res);
+
+  if (!image.body) {
+    res.end()
+    return
+  }
+
+  const reader = image.body.getReader()
+  while (true) {
+    const result = await reader.read()
+    if (result.done) {
+      res.end()
+      return
+    }
+
+    res.write(result.value)
+  }
 }
--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,12 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        buildCommand: "npm run openbuild",
+        environment: {
+          USE_STREAMING_RESPONSE: "true",
+        }
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
